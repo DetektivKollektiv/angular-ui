c152adfcd202d4538f88e98a20cbaf879e20c952
SandroGlueck <s_glueck@gmx.de>
2020-04-30 15:09:37 +0200

    Fixed Amplify

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import 'hammerjs';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import Amplify, {Auth} from 'aws-amplify';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 import amplify from './aws-exports';
 
+import Amplify, {Auth} from 'aws-amplify';
+
 Amplify.configure(amplify);
-
-const oauth = {
-  domain: 'detective-collective.auth.eu-central-1.amazoncognito.com',
-  scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-  redirectSignIn: 'http://localhost:4200/',
-  redirectSignOut: 'http://localhost:4200/',
-  responseType: 'code',
-};
-
-Auth.configure({oauth});
+Auth.configure(amplify);
 
 if (environment.production) {
   enableProdMode();
