cdb74959ce610cd8f45f42559911da5e2e16eb54
SandroGlueck <s_glueck@gmx.de>
2020-06-17 19:33:33 +0200

    Updated environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   api: {
-    baseUrl: 'https://dev.api.detective-collective.org/'
+    baseUrl: 'https://api.dev.detective-collective.org/'
   },
   auth: {
     identityPoolId: 'eu-central-1:69722fe9-a444-4aa5-8016-129a273b2fa5' ,
     userPoolId: 'eu-central-1_edLkAIQVL',
     clientId: '5duuursqtc32rqksh9d00ciplo',
-    authDomain: 'dev.auth.detective-collective.org',
+    authDomain: 'auth.dev.detective-collective.org',
     redirectSignIn: 'https://dev.detective-collective.org/',
     redirectSignOut: 'https://dev.detective-collective.org/'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   api: {
-    baseUrl: 'https://api.detective-collective.org/stage'
+    baseUrl: 'https://api.dev.detective-collective.org/'
   },
   auth: {
     identityPoolId: 'eu-central-1:69722fe9-a444-4aa5-8016-129a273b2fa5' ,
     userPoolId: 'eu-central-1_edLkAIQVL',
     clientId: '5duuursqtc32rqksh9d00ciplo',
-    authDomain: 'dev.auth.detective-collective.org',
+    authDomain: 'auth.dev.detective-collective.org',
     redirectSignIn: 'http://localhost:4200/',
     redirectSignOut: 'http://localhost:4200/'
   }
