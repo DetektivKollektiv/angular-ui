8c33f7365e321edd8e9dc78627b533cc86becdc3
Davide Maiolo <davide.maiolo@w-ays.de>
2020-09-30 22:07:14 +0200

    ml assistant design + funktion. app file wurdegeändert und in submit item breite angepasst (api get des factchecks fehlerhaft)

---
+++ src/app/review-item/services/factchecks/factcheck.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {API} from 'aws-amplify';
+import { Factcheck } from '../../model/factcheck';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FactcheckService {
+
+  private factchecksUrl = '/factchecks';
+
+  constructor() { }
+
+  public getFactcheck(caseId): Promise<Factcheck> {
+    return API.get('api',this.factchecksUrl+'/'+caseId, {}).then((value: Factcheck) => {
+      return value;
+    });
+  }
+}
