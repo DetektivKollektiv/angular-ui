22b48f1c4359dae49c38b501f14658a2ddd940bf
Patrick Mueller <patrick96mueller@gmail.com>
2021-01-08 19:46:48 +0100

    - Profile sidebar - Scalable home component

--- src/app/profile/components/profile/profile.component.ts
+++ src/app/profile/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {UserService} from '../../../core/services/user/user.service';
-import {User} from '../../../core/model/user';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../../../core/services/user/user.service';
+import { User } from '../../../core/model/user';
 
 @Component({
   selector: 'app-profile',
@@ -10,12 +10,14 @@ import {User} from '../../../core/model/user';
 export class ProfileComponent implements OnInit, OnDestroy {
   public user: User;
 
+
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.userService.user$.subscribe(value => {
-      if(value) {
+      if (value) {
         this.user = value;
+        // this.user.progress = 20;
       }
     });
   }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './components/profile/profile.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 
 
@@ -10,7 +11,8 @@ import { ProfileComponent } from './components/profile/profile.component';
         ProfileComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        MaterialModule
     ]
 })
 export class ProfileModule { }
