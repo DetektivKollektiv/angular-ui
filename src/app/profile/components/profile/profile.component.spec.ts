78776a06778bb03479f7ccb99333823b817d6e37
Patrick Mueller <patrick96mueller@gmail.com>
2021-01-09 13:17:42 +0100

    Semicolon again

--- src/app/profile/components/profile/profile.component.spec.ts
+++ src/app/profile/components/profile/profile.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
-import { MaterialModule } from '../../../shared/material/material.module'
+import { MaterialModule } from '../../../shared/material/material.module';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
