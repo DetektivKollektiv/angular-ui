82fb018fce4872e7c863b190df79890baae9cf2a
Patrick Mueller <patrick96mueller@gmail.com>
2021-01-09 13:00:41 +0100

    Missing semicolon *eye-roll-emoji*

--- src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.spec.ts
+++ src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HighScoreSidebarComponent } from './high-score-sidebar.component';
-import { MaterialModule } from '../../../shared/material/material.module'
+import { MaterialModule } from '../../../shared/material/material.module';
 
 describe('HighscoresSidebarComponent', () => {
   let component: HighScoreSidebarComponent;
