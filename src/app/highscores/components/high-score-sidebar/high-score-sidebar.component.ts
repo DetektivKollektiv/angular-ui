28aa9e75d23dd01333d67e14fe22a4c040c3e926
Julian Orinyol <julianorinyol@gmail.com>
2021-02-15 23:03:53 +0200

    fetch and display top users in high-score-sidebar

--- src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.ts
+++ src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.ts
@@ -1,21 +1,45 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../../core/services/user/user.service';
+import {User} from '../../../core/model/user';
+
+type UserWithPosition = User & { position: number};
 
 @Component({
   selector: 'app-high-score-sidebar',
   templateUrl: './high-score-sidebar.component.html',
   styleUrls: ['./high-score-sidebar.component.scss']
 })
+
+
 export class HighScoreSidebarComponent implements OnInit {
   public expanded: boolean;
   public index: number;
+  public usersList: UserWithPosition[] =  [];
+  private noUsers: boolean;
 
-
-  // TODO: Add service to query high scores
-  constructor() {
+  constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
     this.expanded = false;
+
+    this.userService.getTopUsers()
+      .then((users: User[]) => {
+        const usersWithPosition: UserWithPosition[] = users.map((user: User, i: number) => ({
+          ...user,
+          position: i + 1
+        }));
+
+        if (users === null){
+          this.noUsers = true;
+        } else {
+          this.noUsers = false;
+          this.usersList = usersWithPosition;
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   expand() {
