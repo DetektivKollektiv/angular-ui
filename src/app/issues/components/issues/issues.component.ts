725b6340c39c2b7bcffebfcd8d2a457739957b3b
Patrick Mueller <patrick96mueller@gmail.com>
2021-02-15 10:35:19 +0100

    Added honeypot checkbox Navigating back to home after submission

--- src/app/issues/components/issues/issues.component.ts
+++ src/app/issues/components/issues/issues.component.ts
@@ -5,6 +5,7 @@ import { LoaderService } from '../../../shared/loader/service/loader.service';
 import { IssueService } from '../../services/issue.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Logger } from 'aws-amplify';
+import { Router } from '@angular/router';
 
 
 
@@ -18,6 +19,7 @@ export class IssuesComponent implements OnInit {
 
   categoryFormControl: FormControl;
   messageFormControl: FormControl;
+  checkboxFormControl: FormControl;
 
   submitted: boolean;
 
@@ -25,27 +27,38 @@ export class IssuesComponent implements OnInit {
     private loaderService: LoaderService,
     private issueService: IssueService,
     private snackBar: MatSnackBar,
-    private formBuilder: FormBuilder
-
+    private formBuilder: FormBuilder,
+    private router: Router
 
   ) { }
 
   ngOnInit(): void {
-    this.categoryFormControl = new FormControl('', Validators.required);
-    this.messageFormControl = new FormControl('', Validators.required);
-
+    this.categoryFormControl = new FormControl(null, Validators.required);
+    this.messageFormControl = new FormControl(null, Validators.required);
+    this.checkboxFormControl = new FormControl();
     this.issueFormGroup = this.formBuilder.group({
       categoryFormControl: this.categoryFormControl,
-      messageFormControl: this.messageFormControl
+      messageFormControl: this.messageFormControl,
+      checkboxFormControl: this.checkboxFormControl
     });
   }
 
   submit() {
-    const logger = new Logger('foo');
-
+    if (this.checkboxFormControl.value === true) {
+      this.navigate('/');
+      this.snackBar.open(
+        'Deine Nachricht wurde versendet. Wir kümmern uns schnellstmöglich um dein Anliegen!',
+        'Ok',
+        {
+          duration: 3000,
+        }
+      );
+      return;
+    }
     const issue = {
       category: this.categoryFormControl.value,
       message: this.messageFormControl.value,
+      // TODO: Load item_id dynamically when component is loaded from archive
       // item_id: ;
     } as Issue;
     this.loaderService.show();
@@ -54,18 +67,28 @@ export class IssuesComponent implements OnInit {
       .then((_) => {
         this.submitted = true;
         this.loaderService.hide();
-      })
-      .catch((e) => {
-        logger.error('error happened', e);
-        this.loaderService.hide();
+        this.navigate('/');
         this.snackBar.open(
-          'Deine Nachricht konnte nicht versendet werden. Versuch es später nochmal.',
+          'Deine Nachricht wurde versendet. Wir kümmern uns schnellstmöglich um dein Anliegen!',
           'Ok',
           {
             duration: 2000,
           }
         );
+      })
+      .catch((_) => {
+        this.loaderService.hide();
+        this.snackBar.open(
+          'Deine Nachricht konnte nicht versendet werden. Versuch es später nochmal.',
+          'Ok',
+          {
+            duration: 3000,
+          }
+        );
       });
+  }
+  navigate(url: string) {
+    this.router.navigateByUrl(url).then().catch();
   }
 
 }
