ef6eabc01f2c0aa384417cd21961a7c7198997ba
Patrick Mueller <patrick96mueller@gmail.com>
2021-02-15 11:18:08 +0100

    Removed Recaptcha

--- src/app/archive/archive.module.ts
+++ src/app/archive/archive.module.ts
@@ -1,21 +1,18 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MaterialModule} from '../shared/material/material.module';
-import {RecaptchaFormsModule, RecaptchaModule} from 'ng-recaptcha';
-import {HelperModule} from '../shared/helper/helper.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../shared/material/material.module';
+import { HelperModule } from '../shared/helper/helper.module';
 import { ArchiveComponent } from './components/archive/archive.component';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ArchiveComponent],
-    imports: [
-        CommonModule,
-        MaterialModule,
-        RecaptchaModule,
-        RecaptchaFormsModule,
-        HelperModule,
-        RouterModule,
-    ]
+  imports: [
+    CommonModule,
+    MaterialModule,
+    HelperModule,
+    RouterModule,
+  ]
 })
 export class ArchiveModule {
 }
