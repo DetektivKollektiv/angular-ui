a0d4f09cbe1affc463885d0d92c4dc594ecf87af
Patrick Mueller <patrick96mueller@gmail.com>
2021-03-15 15:00:23 +0100

    - Added leaderboard model and service - Adapted highscore component

--- src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.ts
+++ src/app/highscores/components/high-score-sidebar/high-score-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../core/services/user/user.service';
+import { LeaderboardService } from '../../services/leaderboard.service';
+import { Leaderboard } from '../../model/leaderboard';
 import { User } from '../../../core/model/user';
 
 
@@ -13,28 +15,38 @@ import { User } from '../../../core/model/user';
 export class HighScoreSidebarComponent implements OnInit {
   public expanded: boolean;
   public index: number;
-  public userList: User[] = [];
+  // public userList: User[] = [];
+  public leaderboard: Leaderboard;
   private noUsers: boolean;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private leaderboardService: LeaderboardService) {
   }
 
   ngOnInit(): void {
     this.expanded = false;
 
-    this.userService.getTopUsers()
-      .then((users: User[]) => {
-        if (users === null) {
-          this.noUsers = true;
-        } else {
-          this.noUsers = false;
-          this.userList = users;
-        }
+    this.leaderboardService.getTopUsers()
+      .then((leaderboard: Leaderboard) => {
+        this.leaderboard = leaderboard;
       })
       .catch(err => {
         console.error(err);
       });
   }
+
+  //   this.userService.getTopUsers()
+  //     .then((users: User[]) => {
+  //       if (users === null) {
+  //         this.noUsers = true;
+  //       } else {
+  //         this.noUsers = false;
+  //         this.userList = users;
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // }
 
   expand() {
     if (!this.expanded) {
--- src/app/highscores/model/leaderboard.ts
+++ src/app/highscores/model/leaderboard.ts
@@ -1,6 +1,6 @@
 import { User } from '../../core/model/user';
 
-export interface Issue {
+export interface Leaderboard {
     top_users: [User];
     top_users_by_level: [User];
     top_users_by_period: [User];
