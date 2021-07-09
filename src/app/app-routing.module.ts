6aec225af9083fdd31389b0188ac102489476211
Julian Orinyol <julianorinyol@gmail.com>
2021-07-09 21:54:16 +0200

    set up basic login page at /login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/components/home/home.component';
 import { SubmitComponent } from './submit-item/components/submit/submit.component';
 import { ReviewComponent } from './review-item/components/review/review.component';
 import { ArchiveComponent } from './archive/components/archive/archive.component';
+import { LoginPageComponent } from './login/components/loginPage.component';
 import { CommunityGuidelinesComponent } from './core/components/community-guidelines/community-guidelines.component';
 import { UnsavedChangesGuard } from './shared/unsaved-changes/guard/unsaved-changes.guard';
 import { MyFileComponent } from './my-file/components/my-file/my-file.component';
@@ -25,6 +26,10 @@ const routes: Routes = [
   {
     path: 'archive',
     component: ArchiveComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
   },
   {
     path: 'archive/:id',
---
+++ src/app/login/components/loginPage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './loginPage.component.html',
+  styleUrls: ['./loginPage.component.scss'],
+})
+
+export class LoginPageComponent implements OnInit {
+  constructor(
+  ) {}
+
+  ngOnInit(): void {
+  }
+}
