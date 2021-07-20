b4eaa5c48c7ad698b441ba71b6b8c623228ac9f0
vitalikvaraksa <vitalikvaraksa@mail.ru>
2021-07-20 13:41:18 +0300

    show each menu dialog in logged state and not logged state

--- src/app/core/dialogs/menu-dialog/menu.component.ts
+++ src/app/core/dialogs/menu-dialog/menu.component.ts
@@ -1,5 +1,9 @@
-import {Component, Inject } from '@angular/core';
+import {Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { AuthState } from '../../../shared/auth/model/auth-state';
+import { AuthService } from '../../../shared/auth/auth-service/auth.service';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../model/user';
 
 export interface DialogData {
   animal: string;
@@ -12,11 +16,32 @@ export interface DialogData {
   styleUrls: ['./menu.component.scss'],
 })
 
-export class MenuDialogComponent {
+export class MenuDialogComponent implements OnInit {
+  public authState: AuthState;
+  public user: User;
+  public userLoaded = true;
 
   constructor(
     public dialogRef: MatDialogRef<MenuDialogComponent>,
+    private authService: AuthService,
+    private userService: UserService,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  ngOnInit(): void {
+    this.authService.auth$.subscribe((authState: AuthState) => {
+      this.authState = authState;
+    });
+
+    this.userService.user$.subscribe((user: User) => {
+      if (user) {
+        this.userLoaded = true;
+      } else {
+        this.userLoaded = false;
+      }
+
+      this.user = user;
+    });
+  }
 
   onCancel(): void {
     this.dialogRef.close();
