5876afc4310423d8badc0a9f1d076955482ac567
Julian Orinyol <julianorinyol@gmail.com>
2021-08-04 18:03:50 +0200

    mobile friendly styles for the case-swiper

--- src/app/shared/open-case-list-slider/case-swiper/case-swiper.component.ts
+++ src/app/shared/open-case-list-slider/case-swiper/case-swiper.component.ts
@@ -23,35 +23,34 @@ export class CasesSwiperComponent implements AfterViewInit {
   }
 
   ngOnInit(): void {
-
   }
 
   ngAfterViewInit() {
     this.swiper = new Swiper('.swiper-container', {
       slidesPerView: 1,
-      spaceBetween: 40,
+      spaceBetween: 50,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      breakpoints: {
-        1024: {
-            slidesPerView: 1,
-            spaceBetween: 40,
-        },
-        768: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-        },
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        }
-      }
+      // breakpoints: {
+      //   1024: {
+      //       slidesPerView: 1,
+      //       spaceBetween: 40,
+      //   },
+      //   768: {
+      //       slidesPerView: 1,
+      //       spaceBetween: 30,
+      //   },
+      //   640: {
+      //       slidesPerView: 1,
+      //       spaceBetween: 20,
+      //   },
+      //   320: {
+      //       slidesPerView: 1,
+      //       spaceBetween: 10,
+      //   }
+      // }
     });
   }
 
