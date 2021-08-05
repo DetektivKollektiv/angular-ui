edc4f56f2c83ebb68149019f992afad0b325f7ec
Julian Orinyol <julianorinyol@gmail.com>
2021-08-05 12:26:20 +0200

    case swiper -> jump to next when ablehnen is clicked

--- src/app/shared/open-case-list-slider/case-swiper/case-swiper.component.ts
+++ src/app/shared/open-case-list-slider/case-swiper/case-swiper.component.ts
@@ -54,6 +54,10 @@ export class CasesSwiperComponent implements AfterViewInit {
     });
   }
 
+  slideNext() {
+    this.swiper.slideNext();
+  }
+
   onSwiper(swiper) {
     console.log(swiper);
   }
