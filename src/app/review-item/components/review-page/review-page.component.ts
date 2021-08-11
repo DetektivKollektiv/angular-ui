be8bc4a329f6d99728e24c9962ee0c40a840f7f6
vitalikvaraksa <vitalikvaraksa@mail.ru>
2021-08-11 21:57:05 +0300

    clear console.log

--- src/app/review-item/components/review-page/review-page.component.ts
+++ src/app/review-item/components/review-page/review-page.component.ts
@@ -285,7 +285,6 @@ export class ReviewPageComponent implements OnInit, UnsavedChanges {
   }
 
   agreePolicy(event) {
-    console.log(event)
     this.policyChecked = event.checked;
     this.checkButtonStatus();
   }
