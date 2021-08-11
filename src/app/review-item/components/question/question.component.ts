62bbcb69db3eeba97f93e692d965d5d55528289e
vitalikvaraksa <vitalikvaraksa@mail.ru>
2021-08-11 21:25:11 +0300

    add submit tag func

--- src/app/review-item/components/question/question.component.ts
+++ src/app/review-item/components/question/question.component.ts
@@ -19,8 +19,11 @@ export class QuestionComponent implements OnInit {
   @Input() public questions: Question[];
   @Input() public index: number;
   @Input() public isChild: boolean;
+  @Input() public parentIndex: number = -1;
   public isShowChild: boolean;
   public childQuestions: Question[] = [];
+  public title: string;
+  public alphbt: string = 'abcdefghijklmnopqrstuvwxyz';
 
   @Output() public valueChange = new EventEmitter();
 
@@ -30,6 +33,11 @@ export class QuestionComponent implements OnInit {
     );
 
     this.isShowChild = false;
+    if (this.parentIndex === -1) {
+      this.title = `Frage ${this.index + 1}`
+    } else {
+      this.title = `Frage ${this.parentIndex + 1}${this.alphbt[this.index]}`
+    }
   }
 
   change(e) {
@@ -45,7 +53,5 @@ export class QuestionComponent implements OnInit {
     if (this.childQuestions.length > 0) {
       this.isShowChild = true
     }
-
-    this.valueChange.emit()
   }
 }
