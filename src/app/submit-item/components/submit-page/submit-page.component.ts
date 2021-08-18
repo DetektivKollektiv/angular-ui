22a8e8a996d0c03a275beee3d090cc243f02bad8
Alok Singh <alok.singh3286@gmail.com>
2021-08-18 20:45:12 +0530

    submit page created

--- src/app/submit-item/components/submit-page/submit-page.component.ts
+++ src/app/submit-item/components/submit-page/submit-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-submit-page',
   templateUrl: './submit-page.component.html',
@@ -9,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class SubmitPageComponent implements OnInit {
+
   public questionPrompts: any[];
+
+  favoriteSeason: string;
+  questionOptions: string[] = [
+    'Per Messenger (z.B. Whatsapp, Telegram oder SMS)',
+    'Über soziale Medien (z.B. Facebook, Twitter, Instagram)',
+    'Beim Surfen im Internet',
+    'Über andere Medien (z.B. Fernsehen, Radio, Zeitung)',
+    'Aus Gesprächen (z.B. mit Freunden oder Familienmitgliedern)',
+    'Sonstiges'
+  ];
+
+  questionOptions2: string[] = [
+    'Per Messenger (z.B. Whatsapp, Telegram oder SMS)',
+    'Über soziale Medien (z.B. Facebook, Twitter, Instagram)',
+  ];
+
+
   constructor(
   ) {}
   ngOnInit(): void {
@@ -17,25 +34,25 @@ export class SubmitPageComponent implements OnInit {
       {
         title: 'Woran erkenne ich eine gute Quelle?',
         description: 'Hier haben wir alles zusammengefasst um dir zu helfen gute Quellen zu erkennen',
-        background: '#ff7268',
+        background: 'color__bittersweet',
         icon: 'fal fa-question-circle'
       },
       {
         title: 'Die Quelle ist nicht mehr abrufbar. Was kann ich tun?',
         description: 'Eine Anleitung für genau solche Fälle findest du auf dieser Seite.',
-        background: '#5f38fa',
+        background: 'color__neon',
         icon: 'fal fa-plus'
       },
       {
         title: 'Kann ich den Fall abgeben?',
         description: 'Ja, das geht. Hier erfährst du wie.',
-        background: '#722ed1',
+        background: 'color__purple',
         icon: 'fal fa-archive'
       },
       {
         title: 'Eine weitere Frage??',
         description: 'Und hier ein weiterer Beschreibungstext, der erklärt, was mich beim Klick darauf erwartet.',
-        background: '#ff7268',
+        background: 'color__bittersweet',
         icon: 'fal fa-meteor'
       },
     ];
