6fe5e64623b6403d73ab419ccf84ce4f5a100586
Julian Orinyol <julianorinyol@gmail.com>
2021-10-15 17:41:50 +0200

    refactor: linting fix 3 warnings, in case they are causing codebuild to fail

--- src/app/shared/case-details/case-details/case-details.component.ts
+++ src/app/shared/case-details/case-details/case-details.component.ts
@@ -13,7 +13,10 @@ export class CaseDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.description = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ';
+    // keeping within the eslint char limit by concating.
+    this.description = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor '
+    + 'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea '
+    + 'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ';
   }
 
 }
