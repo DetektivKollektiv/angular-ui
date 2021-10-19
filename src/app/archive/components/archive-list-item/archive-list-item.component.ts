73ea57653ae843a4ca35e48087f85d64dae655ad
Rene Wolf <rene.wolf@re-wo-dev.com>
2021-10-19 20:42:54 +0200

    :recycle: component cleanup, :sparkles: show only max 5 tags

--- src/app/archive/components/archive-list-item/archive-list-item.component.ts
+++ src/app/archive/components/archive-list-item/archive-list-item.component.ts
@@ -1,40 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../../../model/item';
 import { RatingColorService } from '../../../services/rating-color-service/rating-color.service';
 
 @Component({
   selector: 'app-archive-list-item',
   templateUrl: './archive-list-item.component.html',
-  styleUrls: ['./archive-list-item.component.scss'],
+  styleUrls: ['./archive-list-item.component.scss']
 })
-export class ArchiveListItemComponent implements OnInit {
-  @Input() case: any;
-  public color: string;
+export class ArchiveListItemComponent {
+  @Input() case: Item;
 
-  public result_score: number;
-  public startTime: string;
-  public endTime: string;
-  public taggies = [];
-  public comments = [];
-  public linkUrl: string;
-  constructor(private ratingColorService: RatingColorService) { }
-
-  ngOnInit(): void {
-    this.result_score = Math.ceil(this.case.result_score * 25);
-
-
-    this.color = this.ratingColorService.getColorForScore(this.result_score);
-
-
-    if ('comments' in this.case && this.case.comments) {
-      this.comments = this.case.comments;
-    }
-
-    const startTimeArr = this.case.open_timestamp.split(' ')[0].split('-');
-    this.startTime = `${startTimeArr[2]}.${startTimeArr[1]}.${startTimeArr[0]}`;
-    const endTimeArr = this.case.close_timestamp.split(' ')[0].split('-');
-    this.endTime = `${endTimeArr[2]}.${endTimeArr[1]}.${endTimeArr[0]}`;
-
-    this.linkUrl = `/archive/${this.case.id}`;
+  get tagsCount(): number {
+    const { tags, warning_tags } = this.case;
+    return tags.length + warning_tags.length <= 5 ? tags.length : Math.min(tags.length, 3);
   }
 
+  get warningTagsCount(): number {
+    const { warning_tags } = this.case;
+    return Math.min(5 - this.tagsCount, warning_tags.length);
+  }
+
+  constructor(private ratingColorService: RatingColorService) {}
 }
