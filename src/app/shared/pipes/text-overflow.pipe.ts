2f11ad06a0b4240fda486d490ecf94153f72afea
René Wolf <rene.wolf@optimal-systems.de>
2021-11-01 09:29:26 +0100

    handle negative content length in text overflow pipe

--- src/app/shared/pipes/text-overflow.pipe.ts
+++ src/app/shared/pipes/text-overflow.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TextOverflowPipe implements PipeTransform {
   transform(value: string, maxLength: number): string {
-    return value?.length > maxLength ? `${value.substr(0, maxLength)}...` : value;
+    return value?.length > maxLength && maxLength > 0 ? `${value.substr(0, maxLength)}...` : value;
   }
 }
