2e8a73203eb4874fc2489b5dd1b298509cf5b369
Rene Wolf <rene.wolf@re-wo-dev.com>
2021-11-07 20:18:42 +0100

    :bug: fixed question carousel for custom icon colors

--- src/app/review-item/components/review-page/review-page.component.ts
+++ src/app/review-item/components/review-page/review-page.component.ts
@@ -50,25 +50,25 @@ export class ReviewPageComponent implements OnInit {
     {
       title: 'Woran erkenne ich eine gute Quelle?',
       description: 'Hier haben wir alles zusammengefasst um dir zu helfen gute Quellen zu erkennen',
-      background: '#68a8ff',
+      bgColor: '#68a8ff',
       icon: 'fal fa-newspaper'
     },
     {
       title: 'Die Quelle ist nicht mehr abrufbar. Was kann ich tun?',
       description: 'Eine Anleitung für genau solche Fälle findest du auf dieser Seite.',
-      background: '#3a9832',
+      bgColor: '#3a9832',
       icon: 'fal fa-scroll-old'
     },
     {
       title: 'Kann ich den Fall abgeben?',
       description: 'Ja, das geht. Hier erfährst du wie.',
-      background: '#be9843',
+      bgColor: '#be9843',
       icon: 'fal fa-hands-helping'
     },
     {
       title: 'Eine weitere Frage??',
       description: 'Und hier ein weiterer Beschreibungstext, der erklärt, was mich beim Klick darauf erwartet.',
-      background: '#8f1fff',
+      bgColor: '#8f1fff',
       icon: 'fal fa-leaf'
     }
   ];
