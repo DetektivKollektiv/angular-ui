6043b59bd3fed15e9091b023b8dc2895d1185790
René Wolf <rene.wolf@re-wo-dev.com>
2021-11-30 22:21:30 +0100

    :bug: fixed validation for open review questions

--- src/app/review-item/components/question/question.component.ts
+++ src/app/review-item/components/question/question.component.ts
@@ -17,13 +17,11 @@ export class QuestionComponent implements OnInit, OnDestroy {
   @Input() parentIndex = -1;
   @Input() parentFormGroup: FormGroup;
 
-  @Output() valueChange = new EventEmitter();
-
   isShowChild: boolean;
   childQuestions: ItemReviewQuestion[] = [];
   visibleChildQuestions: ItemReviewQuestion[] = [];
   title: string;
-  questionForm: FormControl;
+  questionFormControl: FormControl;
 
   private alphbt = 'abcdefghijklmnopqrstuvwxyz';
   private formSubscription: Subscription;
@@ -47,7 +45,45 @@ export class QuestionComponent implements OnInit, OnDestroy {
     }
   }
 
-  addChildQuestions(): void {
+  showChildQuestions(value): void {
+    if (value === null || value === undefined) {
+      return;
+    }
+
+    this.visibleChildQuestions = [];
+    this.isShowChild = false;
+
+    const questions = this.questions.filter((q) => q.question_id === this.question.question_id);
+
+    this.childQuestions.forEach((question) => {
+      const valueInBound = questions.find((q) => question.lower_bound <= q.answer_value && question.upper_bound >= q.answer_value);
+
+      if (valueInBound) {
+        this.isShowChild = true;
+        this.visibleChildQuestions.push(question);
+
+        this.addFormControl(question);
+      } else {
+        this.removeFormControl(question);
+      }
+    });
+  }
+
+  private addFormControl(question: ItemReviewQuestion) {
+    if (!this.parentFormGroup) {
+      return;
+    }
+
+    const childQuestionControl = this.formBuilder.control(question.answer_value, Validators.required);
+    this.parentFormGroup.addControl(question.question_id, childQuestionControl);
+  }
+
+  private removeFormControl(question: ItemReviewQuestion) {
+    question.answer_value = null;
+    this.parentFormGroup.removeControl(question.question_id);
+  }
+
+  private addChildQuestions(): void {
     const hasChildren = this.question.max_children > 0;
     if (!hasChildren) {
       return;
@@ -62,43 +98,16 @@ export class QuestionComponent implements OnInit, OnDestroy {
     });
   }
 
-  showChildQuestions(value): void {
-    if (value === null || value === undefined) {
-      return;
-    }
-
-    this.visibleChildQuestions = [];
-    this.isShowChild = false;
-
-    const questions = this.questions.filter((q) => q.question_id === this.question.question_id);
-
-    this.childQuestions.forEach((question) => {
-      const valueInBound = questions.find((q) => question.lower_bound <= q.answer_value && question.upper_bound >= q.answer_value);
-      // const valueInBound = question.lower_bound <= value && question.upper_bound >= value;
-
-      if (valueInBound) {
-        this.isShowChild = true;
-        this.visibleChildQuestions.push(question);
-      }
-    });
-  }
-
-  onQuestionValueChange(e: number): void {
-    this.valueChange.emit();
-    this.showChildQuestions(e);
-  }
-
   private handleQuestionForm(): void {
     if (!this.parentFormGroup) {
       return;
     }
 
-    this.questionForm = this.formBuilder.control(this.question.answer_value, Validators.required);
-    this.parentFormGroup.addControl(this.question.question_id, this.questionForm);
+    this.questionFormControl = this.parentFormGroup.get(this.question.question_id) as FormControl;
 
-    this.formSubscription = this.questionForm.valueChanges.pipe().subscribe((value) => {
+    this.formSubscription = this.questionFormControl.valueChanges.pipe().subscribe((value) => {
       this.question.answer_value = value;
-      this.onQuestionValueChange(value);
+      this.showChildQuestions(value);
     });
   }
 }
