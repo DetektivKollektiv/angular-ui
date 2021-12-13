83708168e302b8c39b63bd76de68b39be6df31ed
Sandro Glück <38329766+SandroGlueck@users.noreply.github.com>
2021-12-13 22:26:34 +0100

    Incorporated most requested changes from review

--- src/app/static-pages/about/about.component.spec.ts
+++ src/app/static-pages/about/about.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BreadcrumbModule } from '@shared/breadcrumb/breadcrumb.module';
+import { CallToActionSectionComponent } from '../landing-page/sections/call-to-action-section/call-to-action-section.component';
+import { SupportUsSectionComponent } from '../landing-page/sections/support-us-section/support-us-section.component';
+import { SupporterItemComponent } from '../landing-page/sections/supporter-section/supporter-item/supporter-item.component';
+import { SupporterSectionComponent } from '../landing-page/sections/supporter-section/supporter-section.component';
 
 import { AboutComponent } from './about.component';
 
@@ -8,9 +15,15 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        AboutComponent,
+        SupporterSectionComponent,
+        SupportUsSectionComponent,
+        CallToActionSectionComponent,
+        SupporterItemComponent
+      ],
+      imports: [BreadcrumbModule, RouterTestingModule, HttpClientModule]
+    }).compileComponents();
   });
 
   beforeEach(() => {
