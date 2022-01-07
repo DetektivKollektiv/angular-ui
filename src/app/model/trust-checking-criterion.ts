d43aa17e7c6831fbd2d73cb096f73dba91327bb4
PotSchmoell <patrick96mueller@gmail.com>
2022-01-07 14:26:03 +0100

    Finished trust-checking page

--- src/app/model/trust-checking-criterion.ts
+++ src/app/model/trust-checking-criterion.ts
@@ -3,6 +3,5 @@ export interface TrustCheckingCriterion {
     description: string;
     tag_text: string;
     icon: string;
-    tag_bg_color: string;
-    tag_text_color: string;
+    text_color: string;
 }
