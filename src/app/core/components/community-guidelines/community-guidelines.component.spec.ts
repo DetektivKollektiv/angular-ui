cb0127592002b22bafeef59df910094edb939b22
ChristianBay <80820320+ChristianBay@users.noreply.github.com>
2022-01-26 17:39:50 +0100

    #172 added RouterTestingModule import

--- src/app/core/components/community-guidelines/community-guidelines.component.spec.ts
+++ src/app/core/components/community-guidelines/community-guidelines.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommunityGuidelinesComponent } from './community-guidelines.component';
 import { BreadcrumbModule } from '@shared/breadcrumb/breadcrumb.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CommunityGuidelinesComponent', () => {
   let component: CommunityGuidelinesComponent;
@@ -10,7 +11,10 @@ describe('CommunityGuidelinesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CommunityGuidelinesComponent ],
-      imports: [ BreadcrumbModule ],
+      imports: [
+        BreadcrumbModule,
+        RouterTestingModule.withRoutes([]),
+      ],
     })
     .compileComponents();
   });
