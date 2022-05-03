d60614709e6de79fb943f4f6529017111895da38
PotSchmoell <patrick96mueller@gmail.com>
2022-05-03 10:48:29 +0200

    Switched to using angular component for leaderboard slider

--- src/app/shared/leaderboard-slider/leaderboard-slider.module.ts
+++ src/app/shared/leaderboard-slider/leaderboard-slider.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeaderboardSliderComponent } from './leaderboard-slider/leaderboard-slider.component';
 import { LeaderboardElementComponent } from './leaderboard-element/leaderboard-element.component';
-
+import { SwiperModule } from 'swiper/angular';
 
 
 @NgModule({
   declarations: [LeaderboardSliderComponent, LeaderboardElementComponent],
   exports: [LeaderboardSliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule
   ]
 })
 export class LeaderboardSliderModule { }
--- src/app/shared/leaderboard-slider/leaderboard-slider/leaderboard-slider.component.ts
+++ src/app/shared/leaderboard-slider/leaderboard-slider/leaderboard-slider.component.ts
@@ -1,19 +1,17 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import SwiperCore, { Navigation, Pagination, A11y, Swiper } from 'swiper/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Leaderboard } from 'src/app/core/model/leaderboard';
 import { UserService } from 'src/app/core/services/user/user.service';
+import { SwiperComponent } from 'swiper/angular';
 
-SwiperCore.use([Navigation, Pagination, A11y]);
 @Component({
   selector: 'app-leaderboard-slider',
   templateUrl: './leaderboard-slider.component.html',
   styleUrls: ['./leaderboard-slider.component.scss']
 })
-export class LeaderboardSliderComponent implements OnInit, AfterViewInit {
+export class LeaderboardSliderComponent implements OnInit {
+  @ViewChild('swiper', {static: false}) swiper?: SwiperComponent;
   leaderboard: Leaderboard;
-  category = 0;
   content = [];
-  swiper: Swiper;
 
   constructor(private user_service: UserService) {}
 
@@ -37,39 +35,11 @@ export class LeaderboardSliderComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit(): void {
-    this.swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 50,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      loop: true
-    });
-    this.swiper.on('click', (swiper, event) => {
-      // workaround for not clickable buttons on loop
-      if (!swiper.isBeginning && !swiper.isEnd) {
-        return;
-      }
-      if ((event.target as HTMLElement).classList.contains('slide-next')) {
-        swiper.slideNext();
-        return;
-      }
-      if ((event.target as HTMLElement).classList.contains('slide-prev')) {
-        swiper.slidePrev();
-        return;
-      }
-    });
-  }
-
   slide(next: boolean) {
     if (next) {
-      this.category = this.category < 2 ? this.category + 1 : 0;
-      this.swiper.slideNext();
+      this.swiper.swiperRef.slideNext();
     } else {
-      this.category = this.category > 0 ? this.category - 1 : 2;
-      this.swiper.slidePrev();
+      this.swiper.swiperRef.slidePrev();
     }
   }
 }
