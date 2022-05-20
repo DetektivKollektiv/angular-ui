4caff107f8520730b435c4df2dfc1570fd1c374e
PotSchmoell <patrick96mueller@gmail.com>
2022-05-20 16:18:18 +0200

    Adressed review comments

--- src/app/shared/leaderboard-slider/leaderboard-element/leaderboard-element.component.ts
+++ src/app/shared/leaderboard-slider/leaderboard-element/leaderboard-element.component.ts
@@ -7,21 +7,28 @@ import { User } from 'src/app/core/model/user';
   styleUrls: ['./leaderboard-element.component.scss']
 })
 export class LeaderboardElementComponent implements OnInit {
-
   @Input() rank: number;
   @Input() user: User;
 
   height;
   medalColorClass;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    switch(this.rank){
-      case 1: this.height='70px';this.medalColorClass='goldMedal';break;
-      case 2: this.height='55px';this.medalColorClass='silverMedal';break;
-      case 3: this.height='40px';this.medalColorClass='bronzeMedal';break;
+    switch (this.rank) {
+      case 1:
+        this.height = '70px';
+        this.medalColorClass = 'goldMedal';
+        break;
+      case 2:
+        this.height = '55px';
+        this.medalColorClass = 'silverMedal';
+        break;
+      case 3:
+        this.height = '40px';
+        this.medalColorClass = 'bronzeMedal';
+        break;
     }
   }
-
 }
