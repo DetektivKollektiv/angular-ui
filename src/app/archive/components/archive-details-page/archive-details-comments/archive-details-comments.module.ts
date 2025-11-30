e8bd3e6b89c9655fcf4f058430de92846d5037a0
gormlabenz <gorm@labenz.io>
2025-11-30 12:39:12 +0100

    Refactor comments navigation in archive details to use SliderButtonComponent for improved usability and maintainability

--- src/app/archive/components/archive-details-page/archive-details-comments/archive-details-comments.module.ts
+++ src/app/archive/components/archive-details-page/archive-details-comments/archive-details-comments.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@shared/material/material.module';
 import { SwiperModule } from 'swiper/angular';
+import { SliderButtonModule } from '../../../shared/slider-button/slider-button.module';
 import { ArchiveDetailsCommentsComponent } from './archive-details-comments.component';
 
 @NgModule({
   declarations: [ArchiveDetailsCommentsComponent],
-  imports: [CommonModule, MaterialModule, SwiperModule],
+  imports: [CommonModule, MaterialModule, SwiperModule, SliderButtonModule],
   exports: [ArchiveDetailsCommentsComponent]
 })
 export class ArchiveDetailsCommentsModule {}
